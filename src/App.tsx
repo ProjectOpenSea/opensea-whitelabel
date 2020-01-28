8ac0fc4ce9f3ab26fceb40fb0aac28c176df9926
Alexander Atallah <alexander.atallah@gmail.com>
2020-01-28 11:06:09 -0500

    setup OpenSea creature demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ASSET_URL, CHOSEN_THEME } from './constants'
+import logo from './static/images/logo.png';
+import './static/styles/App.css';
+import footer from './static/images/footer.png';
+import { iframeResizer } from 'iframe-resizer'
+import './static/styles/App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const embeddedUrl = `${ASSET_URL}?embed=${CHOSEN_THEME}`
+
+class App extends React.Component {
+
+  componentDidMount() {
+    iframeResizer({ log: false }, '#opensea-iframe')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            
+          </p>
+          <i className="material-icons right">menu</i>
+        </header>
+        <main className="App-main">
+          <div className="App-hero">
+            <div className="App-hero-image"></div>
+            <p>
+              My Marketplace
+            </p>
+            <small>Digital collectibles from my company</small>
+          </div>
+          <iframe id="opensea-iframe" title="Embedded OpenSea Marketplace" src={embeddedUrl} width='100%' height='100%' frameBorder='0' allowFullScreen></iframe>
+        </main>
+        <footer className="App-footer">
+          {/* Placeholder image for footer */}
+          <img style={{width: '100%'}} src={footer} className="footer" alt="footer" />
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
