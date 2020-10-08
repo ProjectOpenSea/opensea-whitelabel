e650979bdfe32278ae6ad3b4fd344ea8990d454c
Alexander Atallah <alexander.atallah@gmail.com>
2020-10-08 15:14:12 -0400

    update themes

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,9 @@
-export const ASSET_URL = 'https://opensea.io/assets/0x1301566b3cb584e550a02d09562041ddc4989b91/28'
+export const ASSET_URL =
+  "https://opensea.io/assets/0x1301566b3cb584e550a02d09562041ddc4989b91/28";
 
 export enum EmbedTheme {
   Default = "true",
   Simple = "simple",
-  Night = "night",
-  Day = "light",
 }
 
-export const CHOSEN_THEME = EmbedTheme.Night;
+export const CHOSEN_THEME = EmbedTheme.Default;
